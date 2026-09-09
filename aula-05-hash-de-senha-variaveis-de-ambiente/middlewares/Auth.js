b8c9e80b372f024e0a94d14ed9673707a0cfdc03
Camile Dias <camileedias@gmail.com>
2026-09-08 22:06:44 -0300

    Aula 05

---
+++ aula-05-hash-de-senha-variaveis-de-ambiente/middlewares/Auth.js
@@ -0,0 +1,35 @@
+// MIDDLEWARE de autenticação
+import jwt from "jsonwebtoken";
+import userController from "../controllers/userController.js";
+// Função que irá verificar se o usuário possui um token válido
+const Authorization = (req, res, next) => {
+  //  Capturando o token do cabeçalho da requisição
+  const authToken = req.headers["authorization"];
+  // Se o token não for vazio
+  if (authToken != undefined) {
+    const bearer = authToken.split(" ");
+    // Capturando somente o token
+    const token = bearer[1];
+    //  Validando o token o com o JWT
+    jwt.verify(token, userController.JWTSecret, (error, data) => {
+      // Se o token for inválido
+      if (error) {
+        // Cod. 401 (UNAUTHORIZED)
+        res.status(401).json({ error: "Token inválido" });
+        // Se o token for válido
+      } else {
+        req.token = token;
+        req.loggedUser = {
+          id: data.id,
+          email: data.email,
+        };
+        // Permite prosseguir com a requisição
+        next();
+      }
+    });
+    // Se o token estiver vazio
+  } else {
+    res.status(401).json({ error: "Token não informado." });
+  }
+};
+export default { Authorization };
